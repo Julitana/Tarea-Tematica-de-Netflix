a1e633c44955dab53c80ac78df7a764e4bca9c36
Julitana <aangulo@vlacademy.com>
2025-01-25 12:45:09 -0600

    cambiar color oscuro y claro

---
+++ Tarea_Netflix.js
@@ -0,0 +1,44 @@
+console.log("Ejercicios tematicos de Netflix")
+
+
+//3- Zona de Funciones 
+
+  //1. Saludo de Bienvenida
+  function BienvenidaNetflix() {
+    alert('¡Bienvenido a Netflix!')
+    
+  }
+
+//2. Recomendación de Serie
+function RecomendarSerie() {
+  alert("Stranger Things");
+}
+function RecomendarSerie2(serie) {
+   alert("The Witcher");
+}
+
+function MostrarPerfil(nombre,email){
+  alert(`Perfil de Usuario: ${nombre} ${email}`);
+}
+
+//4. Activar Modo Oscuro (Netflix Night Mode)
+function NetflixNightMode(valor1, valor2){ 
+  document.body.style.backgroundColor = valor1;
+  document.body.style.color =valor2;
+}
+
+//5. Activar Modo Claro (Netflix Day Mode)
+function NetflixDayMode  (valor1, valor2){ 
+  document.body.style.backgroundColor = valor1;
+  document.body.style.color = valor2;
+}
+
+//6. Calcular Tiempo de Maratón
+function   () {
+  alert`Tiempo total: ${episodios ? duracion} minutos.`;
+}
+//7. Dividir Episodios en Días
+function {
+  alert(`Episodios por día: ${episodios ? dias}.`);
+}
+
